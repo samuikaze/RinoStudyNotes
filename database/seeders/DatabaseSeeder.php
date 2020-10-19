87c1988f5f08fc3bf7214aac0ec0168e8c0b24e7
SamuiKaze <rr5145@gmail.com>
2020-10-20 03:19:16 +0800

    feat: 補上版本 Seed 資料

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -89,6 +89,11 @@ class DatabaseSeeder extends Seeder
                         'content' => json_encode(['完成新增角色資料功能', '修正部分登入系統行為', '修正驗證登入失敗時重新導向位址不正確問題'], JSON_UNESCAPED_UNICODE),
                         'created_at' => Carbon::parse('2020-10-19 02:30:00'),
                         'updated_at' => Carbon::parse('2020-10-19 02:30:00'),
+                    ], [
+                        'version_id' => '0.0.6',
+                        'content' => json_encode(['完成新增角色相關資料功能', '修正部分登出重新導向問題', 'API 清單試作'], JSON_UNESCAPED_UNICODE),
+                        'created_at' => Carbon::parse('2020-10-20 03:19:00'),
+                        'updated_at' => Carbon::parse('2020-10-20 03:19:00'),
                     ],
                 ],
             ],
